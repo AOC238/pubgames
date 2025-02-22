3aa5aba3968af048d3a97d676d75afa0c7ac4409
Newman251 <lukejamesnewman@gmail.com>
2025-02-22 20:12:15 +0000

    try

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: "/e-plantShopping",  // Keep your base path if needed
   plugins: [react()],
   server: {
     mimeTypes: {
-      'text/jsx': 'application/javascript'
-    }
+      'text/jsx': 'application/javascript',
+    },
+  },
+  esbuild: {
+    loader: 'jsx',
   },
   resolve: {
-    extensions: ['.js', '.jsx']
-  }
-})
+    alias: {
+      './runtimeConfig': './runtimeConfig.browser',
+    },
+    extensions: ['.js', '.jsx'],
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        '.js': 'jsx',
+      },
+    },
+  },
+});
